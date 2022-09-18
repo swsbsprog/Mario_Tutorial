d0c03e6ed969ce2af4c0f37f03acdb0aeb55e0d5
hktrollgames <hktrollgames@gmail.com>
2022-09-18 14:30:50 +0900

    방향키 이동에 따른 이미지 플립

---
+++ Assets/FlipByDirectionX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipByDirectionX : MonoBehaviour
+{
+    SpriteRenderer sp;
+    IMover mover;
+    public bool defaultDirection = true;
+    void Start()
+    {
+        sp = GetComponentInChildren<SpriteRenderer>();
+        mover = GetComponent<IMover>();
+    }
+
+    void Update()
+    {
+        //방향에 따라서 sp를 플립하자.
+        if (mover.DirectionX < 0)
+            sp.flipX = defaultDirection;
+        else if (mover.DirectionX > 0)
+            sp.flipX = !defaultDirection;
+    }
+}
+
+interface IMover
+{
+    float DirectionX { get; }
+}
--- Assets/Mario.cs
+++ Assets/Mario.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mario : MonoBehaviour
+public class Mario : MonoBehaviour, IMover
 {
     public float speed = 3;
+
+    public float DirectionX => horizontal;
+    float horizontal;
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
         if (horizontal > 0)
             horizontal = 1;
         else if(horizontal < 0)
             horizontal = -1;
-        transform.Translate(horizontal * speed * Time.deltaTime, 0, 0);
+        //transform.Translate(horizontal * speed 
+        //    * Time.deltaTime, 0, 0);
+        var pos = transform.position;
+        pos.x += horizontal * speed * Time.deltaTime;
+        transform.position = pos;
     }
 }
