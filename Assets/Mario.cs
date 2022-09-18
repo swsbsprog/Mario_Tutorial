ff7bb99af4b3fb6e0a66d0f8917f3682e2dce36f
hktrollgames <hktrollgames@gmail.com>
2022-09-18 14:24:12 +0900

    캐릭터 움직임

---
+++ Assets/Mario.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario : MonoBehaviour
+{
+    public float speed = 3;
+    public float horizontal;
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        if (horizontal > 0)
+            horizontal = 1;
+        else if(horizontal < 0)
+            horizontal = -1;
+        transform.Translate(horizontal * speed * Time.deltaTime, 0, 0);
+    }
+}
