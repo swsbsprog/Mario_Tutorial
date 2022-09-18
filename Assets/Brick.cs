a822531cb2c53791bf1e27f4777d9e531e1b7bab
hktrollgames <hktrollgames@gmail.com>
2022-09-18 16:41:15 +0900

    벽 점프로 때리면 동전 생성

---
+++ Assets/Brick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject itemGo;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            foreach (var c in collision.contacts)
+            {
+                if (c.collider.transform.name != "Mario")
+                    continue;
+                if (c.normal.y == 1)
+                {
+                    print("마리오가 밑에서 부딪힘");
+                    // 들썩이는 애니메이션 재생.
+                    animator.SetTrigger("Crash"); 
+
+                    // 동전 혹은 아이템 생성.
+                    var newItem = Instantiate(itemGo);
+                    newItem.transform.position = transform.position + new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+}
