db873c8f5392093f94ff994a8eac3c9530e62f17
hktrollgames <hktrollgames@gmail.com>
2022-09-18 15:05:48 +0900

    점프

--- Assets/Mario.cs
+++ Assets/Mario.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Mario : MonoBehaviour, IMover
 {
     public float speed = 3;
-
     public float DirectionX => horizontal;
     float horizontal;
+    public Rigidbody2D rb;
+    public Vector2 jumpForce = new(0, 100);
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
@@ -15,5 +16,11 @@ public class Mario : MonoBehaviour, IMover
             horizontal = 1;
         else if(horizontal < 0)
             horizontal = -1;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // 점프.
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
